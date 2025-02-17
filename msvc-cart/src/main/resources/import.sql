d6008e5e5c6a33aba07be3f09dca952a92fb8369
decouflet <asanchezd@fi.uba.ar>
2025-02-17 20:39:42 -0300

    create user entity

--- msvc-cart/src/main/resources/import.sql
+++ msvc-cart/src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO cart (id, user_id, created_at) VALUES (1, 101, '2025-02-15 10:00:00');
-INSERT INTO cart (id, user_id, created_at) VALUES (2, 102, '2025-02-15 11:00:00');
-INSERT INTO cart (id, user_id, created_at) VALUES (3, 103, '2025-02-15 12:00:00');
+INSERT INTO cart (id, user_id, created_at) VALUES (1, 1, '2025-02-15 10:00:00');
+INSERT INTO cart (id, user_id, created_at) VALUES (2, 2, '2025-02-15 11:00:00');
+INSERT INTO cart (id, user_id, created_at) VALUES (3, 3, '2025-02-15 12:00:00');
 
 
 INSERT INTO cart_product (cart_id, product_id, quantity) VALUES (1, 1, 2);
---
+++ msvc_user/src/main/java/com/microservice/user/msvc_user/entities/User.java
@@ -0,0 +1,22 @@
+package com.microservice.user.msvc_user.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String contraseña;
+    private boolean vip;
+}
