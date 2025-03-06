eeae39b68951ecb685f53f92c845ed4c54208131
decouflet <asanchezd@fi.uba.ar>
2025-03-06 17:27:04 -0300

    added products and users

--- msvc_user/src/main/resources/import.sql
+++ msvc_user/src/main/resources/import.sql
@@ -1,3 +1,7 @@
 INSERT INTO users (id, name, password, vip, spent) VALUES (1, 'Juan Pérez', 'password123', true, 2300);
 INSERT INTO users (id, name, password, vip, spent) VALUES (2, 'María López', 'securePass', false, 4323);
 INSERT INTO users (id, name, password, vip, spent) VALUES (3, 'Carlos García', 'mySecret', true, 1900);
+INSERT INTO users (id, name, password, vip, spent) VALUES (4, 'Carlos Gomez', 'secretPass', false, 0);
+INSERT INTO users (id, name, password, vip, spent) VALUES (5, 'Lucia Perez', 'securePass456', true, 0);
+INSERT INTO users (id, name, password, vip, spent) VALUES (6, 'Andres Martinez', 'pass789', false, 0);
+INSERT INTO users (id, name, password, vip, spent) VALUES (7, 'Mariana Lopez', 'strongPass321', true, 0);
